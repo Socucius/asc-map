66c0cac4d2d120d48a00f84cacd684247b930381
Alexander Shagov <x48j35@gmail.com>
2016-07-24 19:41:47 +0300

    Update active admin configuration

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,8 +4,8 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Asc Map"
-
+  config.site_title = "AIESEC | MAP"
+  config.allow_comments = false
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
