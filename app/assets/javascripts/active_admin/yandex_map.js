80fc0816df3150631646c1d85fc622cd699f2daf
Alexander Shagov <x48j35@gmail.com>
2016-07-24 20:47:31 +0300

    Add placemark to ymap activeadmin form

--- app/assets/javascripts/active_admin/yandex_map.js
+++ app/assets/javascripts/active_admin/yandex_map.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
   ymaps.ready(init);
 
   function init() {
-    map = new ymaps.Map("map", {
+    var lat = $('#map').attr('data-lat')
+    var lon = $('#map').attr('data-lon')
+    var placemark;
+    var map = new ymaps.Map("map", {
         center: [53.9, 27.56],
-        zoom: 12
+        zoom: (lat && lon) ? 14 : 12
     });
+
+    if(lat && lon) {
+      placemark = new ymaps.Placemark([lat, lon], {
+        hintContent: 'Resently added place'
+      });
+      map.geoObjects.add(placemark);
+    }
     map.controls
     .remove('trafficControl')
     .remove('typeSelector')
