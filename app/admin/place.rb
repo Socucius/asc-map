87944eebc57652a445bed92c77cbded8c2900d55
Alexander Shagov <x48j35@gmail.com>
2016-07-24 20:51:10 +0300

    Update active_admin index

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register Place do
   permit_params :title, :description, :latitude, :longitude
   form partial: 'form'
+
+  index do
+    column :title
+    column :created_at
+    actions
+  end
 end
