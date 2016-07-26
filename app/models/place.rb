365465b16f0f7166b5486e19d196ab793362a8d6
Alexander Shagov <x48j35@gmail.com>
2016-07-26 16:06:51 +0300

    Add place collections model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
   has_many :images, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :images, reject_if: :all_blank, allow_destroy: true
+  has_and_belongs_to_many :place_collections
 
   validates :title, :description, :latitude, :longitude, presence: true
 end
