221e90dae2f2067c6d49e6e2ab43c9fc3257e578
Alexander Shagov <x48j35@gmail.com>
2016-07-28 15:53:44 +0300

    Fix category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Category, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:slug) }
     it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:background_image) }
     it { should validate_uniqueness_of(:slug) }
     it { should have_and_belong_to_many(:place_collections) }
   end
